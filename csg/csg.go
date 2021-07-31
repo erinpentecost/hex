4686cba032093adbca2b0a0c2951663247826ff8
Erin Pentecost <erin@pentecost.email>
2021-07-31 00:26:45 -0700

    wip cached bounds

--- csg/csg.go
+++ csg/csg.go
@@ -8,14 +8,29 @@ import (
 	"github.com/erinpentecost/hexcoord/pos"
 )
 
+var (
+	_ Builder = (*Area)(nil)
+	_ Builder = (*AreaBuilder)(nil)
+)
+
 // exists is a dummy object to stick into map[pos.Hex]struct{}s
 // to avoid allocating a bunch of small objects.
 var exists = struct{}{}
 
 // Area is a collection of hexes.
-type Area map[pos.Hex]struct{}
+// TODO: swap for a struct that maintains parallel bounding box
+//type Area map[pos.Hex]struct{}
 
-type op func(a Area, b Area) Area
+// Area is a collection of hexes.
+type Area struct {
+	hexes map[pos.Hex]struct{}
+	// boundsClean is true if the bounding box is ok
+	boundsClean bool
+	// bounding box for the area
+	minR, maxR, minQ, maxQ int
+}
+
+type op func(a *Area, b *Area) *Area
 
 // AreaBuilder allows you to use 2-dimensional constructive solid geometry techniques
 // to build collections of hexes.
@@ -25,10 +40,16 @@ type AreaBuilder struct {
 	o op
 }
 
+type BuildOption byte
+
+const (
+	dontClean = iota
+)
+
 // Builder can be used to build Areas.
 type Builder interface {
 	// Build converts a description of an Area into an actual Area.
-	Build() Area
+	Build(b ...BuildOption) *Area
 	// Union combines all hexes in this Area with another.
 	Union(b Builder) Builder
 	// Intersection returns only those hexes shared by both areas.
@@ -38,21 +59,35 @@ type Builder interface {
 }
 
 // AreaFromSlice converts a slice of hexes into a set.
-func AreaFromSlice(b []pos.Hex) Area {
-	c := make(map[pos.Hex]struct{})
-	for _, k := range b {
-		c[k] = exists
-	}
-	return c
-}
-
-// NewArea creates a new area containing zero or more hexes.
-func NewArea(hexes ...pos.Hex) Area {
+func AreaFromSlice(hexes []pos.Hex) *Area {
 	c := make(map[pos.Hex]struct{})
 	for _, k := range hexes {
 		c[k] = exists
 	}
-	return c
+	minR, maxR, minQ, maxQ := bounds(hexes...)
+	return &Area{
+		hexes: c,
+		minR:  minR,
+		maxR:  maxR,
+		minQ:  minQ,
+		maxQ:  maxQ,
+	}
+}
+
+// NewArea creates a new area containing zero or more hexes.
+func NewArea(hexes ...pos.Hex) *Area {
+	c := make(map[pos.Hex]struct{})
+	for _, k := range hexes {
+		c[k] = exists
+	}
+	minR, maxR, minQ, maxQ := bounds(hexes...)
+	return &Area{
+		hexes: c,
+		minR:  minR,
+		maxR:  maxR,
+		minQ:  minQ,
+		maxQ:  maxQ,
+	}
 }
 
 // NewBuilder creates a new area builder containing zero or more hexes to start with.
@@ -61,22 +96,24 @@ func NewBuilder(hexes ...pos.Hex) Builder {
 }
 
 // Equal returns true if both areas share exactly the same hexes.
-func (a Area) Equal(b Area) bool {
-	if len(a) != len(b) {
+func (a *Area) Equal(b *Area) bool {
+	if len(a.hexes) != len(b.hexes) {
 		return false
 	}
 
-	for k := range a {
-		if _, ok := b[k]; !ok {
+	// TODO: bounding box check
+
+	for k := range a.hexes {
+		if _, ok := b.hexes[k]; !ok {
 			return false
 		}
 	}
 	return true
 }
 
-func (a Area) String() string {
+func (a *Area) String() string {
 	s := []string{}
-	for k := range a {
+	for k := range a.hexes {
 		ks := k.String()
 		i := sort.SearchStrings(s, ks)
 		s = append(s, "")
@@ -86,11 +123,14 @@ func (a Area) String() string {
 	return "Area: {" + strings.Join(s, " ") + "}"
 }
 
-func (a Area) Build() Area {
+func (a *Area) Build(b ...BuildOption) *Area {
+	if len(b) > 0 && !a.boundsClean {
+		// TODO: clean bounds
+	}
 	return a
 }
 
-func (a Area) Union(b Builder) Builder {
+func (a *Area) Union(b Builder) Builder {
 	return &AreaBuilder{
 		a: a,
 		b: b,
@@ -98,7 +138,7 @@ func (a Area) Union(b Builder) Builder {
 	}
 }
 
-func (a Area) Intersection(b Builder) Builder {
+func (a *Area) Intersection(b Builder) Builder {
 	return &AreaBuilder{
 		a: a,
 		b: b,
@@ -106,7 +146,7 @@ func (a Area) Intersection(b Builder) Builder {
 	}
 }
 
-func (a Area) Subtract(b Builder) Builder {
+func (a *Area) Subtract(b Builder) Builder {
 	return &AreaBuilder{
 		a: a,
 		b: b,
@@ -138,59 +178,81 @@ func (ab *AreaBuilder) Subtract(b Builder) Builder {
 	}
 }
 
-func (ab *AreaBuilder) Build() Area {
+func (ab *AreaBuilder) Build(b ...BuildOption) *Area {
 	// Build() allows me to defer iteration until it's needed,
 	// and we can also do things concurrently.
 
-	var b Area
+	var c *Area
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		b = ab.b.Build()
+		c = ab.b.Build()
 	}()
 
 	a := ab.a.Build()
 
 	wg.Wait()
 
-	return ab.o(a, b)
+	return ab.o(a, c)
 }
 
 // union returns all hexes in all areas.
 // this operation is commutative.
-func unionFn(a Area, b Area) Area {
+func unionFn(a *Area, b *Area) *Area {
 	c := make(map[pos.Hex]struct{})
-	for k := range a {
+	for k := range a.hexes {
 		c[k] = exists
 	}
-	for k := range b {
+	for k := range b.hexes {
 		c[k] = exists
 	}
-	return c
+	if a.boundsClean && b.boundsClean {
+		return &Area{
+			hexes:       c,
+			boundsClean: true,
+			minR:        minInt(a.minR, b.minR),
+			maxR:        maxInt(a.maxR, b.maxR),
+			minQ:        minInt(a.minQ, b.minQ),
+			maxQ:        maxInt(a.maxQ, b.maxQ),
+		}
+	}
+	return &Area{
+		hexes: c,
+	}
 }
 
 // intersectionFn returns only those hexes that are in all areas.
 // this operation is commutative.
-func intersectionFn(a Area, b Area) Area {
+func intersectionFn(a *Area, b *Area) *Area {
+
+	// TODO: skip by checking bounds
+
 	c := make(map[pos.Hex]struct{})
-	for k := range b {
-		if _, ok := a[k]; ok {
+	for k := range b.hexes {
+		if _, ok := a.hexes[k]; ok {
 			c[k] = exists
 		}
 	}
-	return c
+	return &Area{
+		hexes: c,
+	}
 }
 
 // subtractFn returns a, but with hexes shared by b removed.
-func subtractFn(a Area, b Area) Area {
+func subtractFn(a *Area, b *Area) *Area {
+
+	// TODO: skip by checking bounds
+
 	c := make(map[pos.Hex]struct{})
 
-	for k := range a {
-		if _, ok := b[k]; !ok {
+	for k := range a.hexes {
+		if _, ok := b.hexes[k]; !ok {
 			c[k] = exists
 		}
 	}
 
-	return c
+	return &Area{
+		hexes: c,
+	}
 }
